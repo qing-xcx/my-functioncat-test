18899d4275a04c13e392202b1cfe2981726dbda3
qing-xcx <2775893083@qq.com>
2025-01-01 18:58:53 +0800

    Update FormsView.vue

--- src/views/FormsView.vue
+++ src/views/FormsView.vue
@@ -22,8 +22,8 @@ const selectOptions = [
 ]
 
 const form = reactive({
-  name: 'John Doe',
-  email: 'john.doe@example.com',
+  name: 'xiugai John Doe',
+  email: 'xiugai john.doe@example.com',
   phone: '',
   department: selectOptions[0],
   subject: '',
